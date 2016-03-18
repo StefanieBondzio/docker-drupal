980b00128b318eb1c799a23bae61905099c72948
Thomas Heyl <thomas.heyl@lwb.de>
2016-03-18 09:42:03 +0100

    add entrypoint

--- drupal-7/docker-entrypoint.sh
+++ drupal-7/docker-entrypoint.sh
@@ -4,3 +4,5 @@ set -e
 sed -i "s/myhostname =.*/myhostname = ${HOST}\.${DOMAIN}/g" /etc/postfix/main.cf
 sed -i "s/mydestination =.*/mydestination = root@${HOST}\.${DOMAIN}, ${HOST}\.${DOMAIN}, localhost\.${DOMAIN}, localhost/g" /etc/postfix/main.cf
 sed -i "s/relayhost =.*/relayhost = ${RELAY}\.${DOMAIN}/g" /etc/postfix/main.cf
+
+exec "$@"
