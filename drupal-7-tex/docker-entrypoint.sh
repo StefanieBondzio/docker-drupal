6ebeb44cc111df1563567fe1f0ced129e71efe47
Thomas Heyl <thomas.heyl@lwb.de>
2016-03-22 16:03:10 +0100

    start postfix with services

--- drupal-7-tex/docker-entrypoint.sh
+++ drupal-7-tex/docker-entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -e
 
-sed -i "s/myhostname =.*/myhostname = ${HOST}\.${DOMAIN}/g" /etc/postfix/main.cf
-sed -i "s/mydestination =.*/mydestination = root@${HOST}\.${DOMAIN}, ${HOST}\.${DOMAIN}, localhost\.${DOMAIN}, localhost/g" /etc/postfix/main.cf
-sed -i "s/relayhost =.*/relayhost = ${RELAY}\.${DOMAIN}/g" /etc/postfix/main.cf
+postconf -e myhostname=${HOST}\.${DOMAIN}
+postconf -e mydestination="root@${HOST}\.${DOMAIN}, ${HOST}\.${DOMAIN}, localhost\.${DOMAIN}"
+postconf -e relayhost=${RELAY}\.${DOMAIN}
 
 exec "$@"
