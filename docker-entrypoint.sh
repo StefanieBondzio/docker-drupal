220f025c696c4bb7cda790468927a5792cae4c18
Thomas Heyl <thomas.heyl@lwb.de>
2016-03-23 07:48:22 +0100

    start postfix with entry.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,7 +2,9 @@
 set -e
 
 postconf -e myhostname=${HOST}\.${DOMAIN}
-postconf -e mydestination="root@${HOST}\.${DOMAIN}, ${HOST}\.${DOMAIN}, localhost\.${DOMAIN}"
+postconf -e mydestination="root@${HOST}.${DOMAIN}, ${HOST}.${DOMAIN}, localhost.${DOMAIN}"
 postconf -e relayhost=${RELAY}\.${DOMAIN}
+
+service postfix restart
 
 exec "$@"
